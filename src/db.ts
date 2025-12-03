0637e90eec3c6bda9ad7d8b18885dd2e7e1b2e4e
Fraol <fam5@calvin.edu>
2025-12-03 10:18:56 -0500

    Fix pg import + install pg types

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 
-export const pool = new pg.Pool({
+export const pool = new Pool({
   host: process.env.DB_SERVER,
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
